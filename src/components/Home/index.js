a13516e2005ea54cd857096773700df99bd2182c
Rakesh <rakeshsagartfi21@gmail.com>
2026-01-05 12:58:21 +0000

    implement responsive Home route UI and Find Jobs navigation - added responsive background images for mobile and desktop - implemented Find Jobs button navigation to /jobs using history - improved layout, typography, and button styling

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,33 @@ import Header from '../Header'
 
 import './index.css'
 
-const Home = () => <Header />
+const Home = props => {
+  const onClickFindJobs = () => {
+    const {history} = props
+    history.replace('/jobs')
+  }
 
-export default Header
+  return (
+    <>
+      <Header />
+      <div className="home-container">
+        <div className="home-content">
+          <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+          <p className="home-description">
+            Millions of people are searching for jobs, salary information,
+            company reviews. Find the job that fits your abilites and potential.
+          </p>
+          <button
+            type="button"
+            className="find-jobs-btn"
+            onClick={onClickFindJobs}
+          >
+            Find Jobs
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
