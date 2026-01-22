a0ba805dc65c43c31053e9644b5879dd88e7f4e1
Rakesh <rakeshsagartfi21@gmail.com>
2026-01-22 14:40:21 +0000

    Add NotFound component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import {Route, BrowserRouter, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
 import JobItem from './components/JobItem'
+import NotFound from './components/NotFound'
 import ProtectedRoute from './components/ProtectedRoute'
 
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/login" component={LoginForm} />
-      <ProtectedRoute exact path="/" component={Home} />
-      <ProtectedRoute exact path="/jobs" component={Jobs} />
-      <ProtectedRoute exact path="/jobs/:id" component={JobItem} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/jobs" component={Jobs} />
+    <ProtectedRoute exact path="/jobs/:id" component={JobItem} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
 )
 
 export default App
