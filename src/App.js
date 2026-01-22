18de0eb47690c56a9b9eec049da5c13a62cb32ff
Rakesh <rakeshsagartfi21@gmail.com>
2026-01-22 14:08:43 +0000

    Implement JobItem component with skills and similar jobs integration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Route, BrowserRouter, Switch} from 'react-router-dom'
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
+import JobItem from './components/JobItem'
 import ProtectedRoute from './components/ProtectedRoute'
 
 import './App.css'
@@ -12,6 +13,7 @@ const App = () => (
       <Route exact path="/login" component={LoginForm} />
       <ProtectedRoute exact path="/" component={Home} />
       <ProtectedRoute exact path="/jobs" component={Jobs} />
+      <ProtectedRoute exact path="/jobs/:id" component={JobItem} />
     </Switch>
   </BrowserRouter>
 )
--- src/components/JobCard/index.js
+++ src/components/JobCard/index.js
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import {FaStar} from 'react-icons/fa'
 import {MdLocationOn} from 'react-icons/md'
 import {BsFillBriefcaseFill} from 'react-icons/bs'
@@ -17,34 +18,40 @@ const JobCard = props => {
     title,
   } = jobDetails
   return (
-    <li className="job-card-item">
-      <div className="img-title-rating">
-        <img src={companyLogoUrl} alt="company logo" className="company-logo" />
-        <div className="title-rating">
-          <h1 className="title">{title}</h1>
-          <div className="star-rating">
-            <FaStar className="star-icon" />
-            <p className="rating">{rating}</p>
+    <Link to={`/jobs/${id}`} className="job-card-link">
+      <li className="job-card-item">
+        <div className="img-title-rating">
+          <img
+            src={companyLogoUrl}
+            alt="company logo"
+            className="company-logo"
+          />
+          <div className="title-rating">
+            <h1 className="title">{title}</h1>
+            <div className="star-rating">
+              <FaStar className="star-icon" />
+              <p className="rating">{rating}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="location-employmenttype-package">
-        <div className="location-employmenttype-container">
-          <div className="location-container">
-            <MdLocationOn className="location-icon" />
-            <p className="location">{location}</p>
+        <div className="location-employmenttype-package">
+          <div className="location-employmenttype-container">
+            <div className="location-container">
+              <MdLocationOn className="location-icon" />
+              <p className="location">{location}</p>
+            </div>
+            <div className="employmenttype-container">
+              <BsFillBriefcaseFill className="briefcase-icon" />
+              <p className="employment-type">{employmentType}</p>
+            </div>
           </div>
-          <div className="employmenttype-container">
-            <BsFillBriefcaseFill className="briefcase-icon" />
-            <p className="employment-type">{employmentType}</p>
-          </div>
+          <p className="package">{packagePerAnnum}</p>
         </div>
-        <p className="package">{packagePerAnnum}</p>
-      </div>
-      <hr className="job-card-hr-line" />
-      <h1 className="description">Description</h1>
-      <p className="job-description">{jobDescription}</p>
-    </li>
+        <hr className="job-card-hr-line" />
+        <h1 className="description">Description</h1>
+        <p className="job-description">{jobDescription}</p>
+      </li>
+    </Link>
   )
 }
 
